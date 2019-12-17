35985f42defe6212e7ee9c830417ca1872dde480
daishinmutaku <daishinmutaku@gmail.com>
2019-12-17 18:08:58 +0900

    intervalの導出

--- SI/selective_inference.py
+++ SI/selective_inference.py
@@ -1,14 +1,17 @@
 import numpy as np
 import param
+from SI import selection_event as se
+from mpmath import mp
+
+LU_res_list = []
 
 def inference(vecX, result):
     H = generate_eta_mat(result)
-    print(H)
     C = generate_c_mat(H)
-    print(C)
     Z = generate_z_mat(C, H, vecX)
-    print(Z)
-    generate_interval(H, C, Z)
+    interval = generate_interval(C, Z)
+    print(interval)
+
 
 def generate_eta_mat(result):
     H = np.zeros((len(result), param.COLOR_RANGE))
@@ -58,11 +61,47 @@ def generate_z_mat(C, H, vecX):
     Z = np.dot(I - np.dot(C, H.T), vecX)
     return Z
 
-def generate_interval(H, C, Z):
-    col = H.shape[0]
-    for i in range(col):
-        c = C[i]
-        z = Z[i]
-        # mergeLU(param.RANGE, param.EPSILON, c, z)
+def generate_interval(C, Z):
+    LU3 = generate_LU_by_vec(se.vecA3, 0, C, Z)
+    print(LU3)
+    LU_list = [LU3]
 
-# def mergeLU(h_r, epsilon, c, z):
+    L = -mp.inf
+    U = mp.inf
+
+    for LU in LU_list:
+        for lu in LU:
+            l = lu[0]
+            u = lu[1]
+            print(l, u)
+            if l <= L and L <= u and u <= U:
+                U = u
+            elif L <= l and l <= U and U <= u:
+                L = l
+            elif L <= l and l <= u and u <= U:
+                L = l
+                U = u
+
+    interval = [L, U]
+    return interval
+
+def generate_LU_by_vec(vecA, b, C, Z):
+    L = -mp.inf
+    U = mp.inf
+
+    for A in vecA:
+        beta = np.dot(A, C)
+        gamma = np.dot(A, Z) + b
+
+        if beta < 0:
+            l = -1 * gamma / beta
+            if l > L:
+                L = l
+        elif beta > 0:
+            u = -1 * gamma / beta
+            if u < U:
+                U = u
+
+    LU = [L, U]
+    LU_list = [LU]
+    return LU_list
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import artificial_data as data
 from Algorithm import meanshift_segmentation as mss
 from SI import selective_inference as si
+from mpmath import mp
+
+mp.dps = 1000
 
 def main():
     vecX = data.generateImageVector();
