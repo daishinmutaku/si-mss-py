2e1cafad0ef57075839502b8f7a87ae8cc452a9b
daishinmutaku <daishinmutaku@gmail.com>
2020-01-24 14:27:01 +0900

    並列処理数の変更

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-for i in `seq 10`; do
-    $i = $i \* 1000
+for i in `seq 40`; do
+    $i = $i \* 250
     echo $i
     nohup /home/omori.y/anaconda3/bin/python ./main.py $i &
 #    nohup /Users/oomoriyumehiro/.pyenv/shims/python ./main.py $i &
